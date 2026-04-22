87909414d67d610361b5c801ec232457682ae1dc
Tudor Hulban <prindex2010@gmail.com>
2026-04-22 19:22:03 +0300

    how to extended start

---
+++ tc_30_how_to_use_extended_test.go
@@ -0,0 +1,60 @@
+package bytearena_test
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/gofiber/fiber/v3"
+	"github.com/stretchr/testify/require"
+	"github.com/tudorhulban/bytearena"
+)
+
+func TestManual(t *testing.T) {
+	writer := os.Stdout
+
+	ingestor, errCrIngestor := bytearena.NewIngestor(
+		bytearena.Size100K(),
+		writer,
+	)
+	require.NoError(t, errCrIngestor)
+	require.NotNil(t, ingestor)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	chIngestionEnd := ingestor.StartIngestion(ctx)
+
+	app := fiber.New()
+
+	app.Get(
+		"/",
+		func(c fiber.Ctx) error {
+			payload := "xxxxxxxxxxxxxxxxxxxx"
+
+			var response string
+
+			bytesWritten, errWrite := ingestor.Write([]byte(payload))
+			if errWrite != nil {
+				response = fmt.Sprintf(
+					"there was error writing: %s",
+					errWrite.Error(),
+				)
+			} else {
+				response = fmt.Sprintf(
+					"wrote: %d bytes with no error",
+					bytesWritten,
+				)
+			}
+
+			return c.SendString(
+				response,
+			)
+		},
+	)
+
+	log.Fatal(app.Listen(":3000"))
+
+	cancel()
+	<-chIngestionEnd
+}
